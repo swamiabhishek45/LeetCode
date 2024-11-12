881eb5f75611b151dc80015bbe89fdd32dcc6b5f
Abhishek Swami <abhishekswami1435@gmail.com>
2024-11-12 11:08:01 +0000

    [LeetCode Sync] Runtime - 2 ms (36.41%), Memory - 8.9 MB (99.96%)

--- my-folder/0125-valid-palindrome/solution.cpp
+++ my-folder/0125-valid-palindrome/solution.cpp
@@ -1,57 +1,34 @@
 class Solution {
-
-private:
-    bool valid(char ch) {
-        if( (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) {
-            return 1;
-        }
-        
-        return 0;
-    }
-    
-    char toLowerCase(char ch) {
-    if( (ch >='a' && ch <='z') || (ch >='0' && ch <='9') )
-        return ch;
-    else{
-        char temp = ch - 'A' + 'a';
-        return temp;
-    }
-}
-    bool checkPalindrome(string a) {
-    int s = 0;
-    int e = a.length()-1;
-
-    while(s<=e) {
-        if(a[s] != a[e])
-        {
-            return 0;       
-        }
-        else{
-            s++;
-            e--;
-        }
-    }
-    return 1;
-}
 public:
+    bool isAlphaNum(char ch){
+        if((ch >= '0' && ch <= '9') || (tolower(ch) >= 'a' && tolower(ch) <= 'z')){
+            return true;
+        }
+
+        return false;
+    }
+
     bool isPalindrome(string s) {
-        
-        //faltu character hatado
-        string temp = "";
-        
-        for(int j=0; j<s.length(); j++) {   
-            if(valid(s[j])) {
-                temp.push_back(s[j]);
+        int st = 0;
+        int e = s.length() - 1;
+
+        while(st < e){
+            if(!isAlphaNum(s[st])){
+                st++;
+                continue;
             }
+
+            if(!isAlphaNum(s[e])){
+                e--;
+                continue;
+            }
+
+            if(tolower(s[st]) != tolower(s[e])){
+                return false;
+            }
+            st++; e--;
         }
-        
-        //lowercase me kardo
-        for(int j=0; j<temp.length(); j++) { 
-            temp[j] = toLowerCase(temp[j]);
-        }
-        
-        //check palindrome
-        return checkPalindrome(temp);
-        
+
+        return true;
     }
 };
