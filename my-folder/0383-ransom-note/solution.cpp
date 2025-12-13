156af09cd70649043a6ce762922f1224ac5aad46
Abhishek Swami <abhishekswami1435@gmail.com>
2025-12-13 17:46:05 +0000

    [LeetCode Sync] Runtime - 3 ms (53.93%), Memory - 11.8 MB (73.22%)

---
+++ my-folder/0383-ransom-note/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> freq(26, 0);
+
+        for(char c: magazine){
+            freq[c - 'a']++;
+        }
+
+        for(char c : ransomNote){
+            freq[c - 'a']--;
+
+            if(freq[c - 'a'] < 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
