2e2e0a3744cdae1e64895cec4e3073023c42233c
Abhishek Swami <abhishekswami1435@gmail.com>
2025-12-13 04:41:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9 MB (8.35%)

---
+++ my-folder/0058-length-of-last-word/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        
+       int i = s.length() - 1;
+       int cnt = 0;
+
+       while(i >= 0 && s[i] == ' '){
+        i--;
+       }
+
+       while(i >= 0 && s[i] != ' '){
+        cnt++;
+        i--;
+       }
+
+
+
+        return cnt;
+    }
+};
