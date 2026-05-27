1913942f7e45964043d5d445e8b0ed90ec8708d2
Abhishek Swami <abhishekswami1435@gmail.com>
2026-05-27 16:47:43 +0000

    [LeetCode Sync] Runtime - 64 ms (76.76%), Memory - 24.4 MB (64.42%)

---
+++ my-folder/3405-count-the-number-of-special-characters-ii/solution.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        vector<int> lower(26, -1);
+        vector<int> upper(26, -1);
+
+        for (int i = 0; i < word.size(); i++) {
+            char ch = word[i];
+
+            if (islower(ch)) {
+                lower[ch - 'a'] = i;
+            } else {
+                int idx = ch - 'A';
+
+                if (upper[idx] == -1) {
+                    upper[idx] = i;
+                }
+            }
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if (lower[i] != -1 &&
+                upper[i] != -1 &&
+                lower[i] < upper[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
